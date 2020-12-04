718ad553e95982146a67299f358aea839f9e6d2e
Maddox Grey <maddoxgrey@Mag-Mell.fios-router.home>
2020-12-04 16:32:55 -0500

    - Added content and layout for home component

--- app/javascript/components/Home/HomeContainer.js
+++ app/javascript/components/Home/HomeContainer.js
@@ -5,17 +5,21 @@ import Updates from "./Updates";
 
 const HomeContainer = () => {
   return (
-    <div className="container">
-      <div className="columns">
-        <img
-          className="img-fluid"
-          src="https://outworld-reptiles-production.s3.amazonaws.com/BabySnek.jpg"
-          alt="Baby snake"
-        ></img>
-      </div>
-      <div className="columns callout">
-        <AboutUs />
-        <Updates />
+    <div className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <img
+              className="img-fluid"
+              src="https://outworld-reptiles-production.s3.amazonaws.com/BabySnek.jpg"
+              alt="Baby snake"
+            ></img>
+          </div>
+        </div>
+        <div className="columns box mt-6">
+          <AboutUs />
+          <Updates />
+        </div>
       </div>
     </div>
   );
--- app/javascript/components/Home/Updates.js
+++ app/javascript/components/Home/Updates.js
@@ -2,28 +2,31 @@ import React from "react";
 
 const Updates = () => {
   return (
-    <div className="column">
-      <h5>2020/2021 Pairings</h5>
-      <div className="pairings">
+    <div className="column callout">
+      <h5 className="is-size-5 has-text-centered">2020/2021 Pairings</h5>
+      <div className="content">
         <ul>
           <li>
-            Lasciel (Hypo Jungle, pos het Albino) x Grimes (Hypo Motley pos het
+            Lasciel (Hypo Jungle pos het Albino) x Grimes (Hypo Motley pos het
             Albino)
           </li>
-          <li>Molly (Hypo het Anery) x Apollo (Hypo het Anery Aby Line)</li>
           <li>
             Boo (Ghost het Albino) x Olasard (Hypo Jungle Motley het Albino)
           </li>
-          <li>Illyria (Jungle het blood) x Spike (Blood Boa)</li>
+          <li>
+            Nyssa (Jungle Arabesque) x Ferrovax (European Line Hypo)
+          </li>
+          <li>
+            Kitana (Maxx Pink Argentine Boa) x Jax (Argentine Boa)
+          </li>
+          <li>Buffy (Blood Boa) x Spike (Blood Boa)</li>
           <li>Angel (DH Blood/Albino Boa) x Faith (DH Blood/Albino Boa)</li>
         </ul>
       </div>
-      <h5>Upcoming Events!</h5>
-      <div className="vending">
-        <ul>
-          <li>No events this year but hopefully in 2021!</li>
-        </ul>
-      </div>
+      <h5 className="is-size-5 has-text-centered">Upcoming Events!</h5>
+      <ul>
+        <li>No events currently planned.</li>
+      </ul>
     </div>
   );
 };
