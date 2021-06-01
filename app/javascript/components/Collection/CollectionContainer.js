9e6be7d4229b762d947b369cd49f75a9b4e98130
Maddox Grey <maddoxgrey42@gmail.com>
2021-05-31 22:53:59 -0400

    Updated logo, made minor changes to Collections

--- app/javascript/components/Collection/CollectionContainer.js
+++ app/javascript/components/Collection/CollectionContainer.js
@@ -93,25 +93,22 @@ const CollectionContainer = () => {
       <div className="container">
         <div className="columns">
           <div className="column">
-            <h1 className="is-size-2">
-              Boa Constrictor Imperator (BCI) Collection
-            </h1>
+            <h1 className="is-size-2">Boa Imperator (BI) Collection</h1>
             <p className="is-size-7">
               If you&apos;re new to the world of boa constrictors you may not be
               aware that there are lots of different types of &quot;boa
               constrictors&quot;. Typically what you see in a pet store or on
               television is referred to as a &quot;boa&quot;, &quot;boa
               constrictor&quot;, or &quot;red-tailed boa&quot;. Chances are
-              pretty good that in all cases the boa in question is a Boa
-              Constrictor Imperator, commonly shortened to BCI in the reptile
-              world. Without getting into too much detail, this species of boa
-              constrictor is found throughout Central and South America and is
-              the most common type of boa in the pet trade. There are a lot of
-              other sub-species and locales of boas! True &quot;red-tail
-              boas&quot; belong to the sub-species Boa Constrictor Constrictor
-              (BCC) and generally have brick red-tails and get larger than
-              BCI&apos;s. There is a fairly good breakdown of the boa species
-              and sub-species here -{" "}
+              pretty good that in all these cases the boa in question is a Boa
+              imperator, commonly shortened to BI in the reptile world. Without
+              getting into too much detail, this species of boa constrictor is
+              found throughout Central and South America and is the most common
+              type of boa in the pet trade. There are a lot of other sub-species
+              and locales of boas! True &quot;red-tail boas&quot; belong to the
+              sub-species Boa Constrictor Constrictor (BCC) and generally have
+              brick red-tails and get larger than BI&apos;s. There is a fairly
+              good breakdown of the boa species and sub-species here -{" "}
               <a
                 href="https://www.reptifiles.com/red-tailed-boa-care/boa-species-subspecies/"
                 target="_blank"
@@ -119,7 +116,7 @@ const CollectionContainer = () => {
               >
                 Reptifiles
               </a>{" "}
-              - if you&apos;re interested in learning more.
+              - if you&apos;re interested in learning more. Also just to note, up until a few years ago the Boa imperator was actually classified as a Boa Constrictor Imperator or BCI for short. Because this change is relatively recent it&apos;s still very common to see Boa imperators referred to as BCI&apos;s or Boa constrictor imperators.
             </p>
             <div className="columns is-multiline section">
               {collectionBCITiles}
@@ -132,21 +129,18 @@ const CollectionContainer = () => {
             <p className="is-size-7">
               Several years ago I saw my first blood boa morph and instantly
               fell in love. It look me a while but I eventually cultivated a
-              very nice collection of Central American BCI&apos;s centered
-              around the blood morph. Central American Boas are still Boa
-              Constrictor Imperators, so they&apos;re they&apos;re technically
+              very nice collection of Central American boas&apos;s centered
+              around the blood morph. Central American boas are still Boa
+              imperators, so they&apos;re they&apos;re technically
               the same species as the boas above. However, boas in Central
-              America evolved different and are considered a &quot;locale&quot;
-              type of boa. They&apos;re much smaller in size than BCI&apos;s
+              America evolved differently and are considered a &quot;locale&quot;
+              type of boa. They&apos;re much smaller in size than BI&apos;s
               from South America and they often have very different patterns.
-              Examples of Central American Boas are Hogg Island Boas, El
-              Salvador Boas (where the &quot;blood morph originated&quot;), and
-              Costa Rica. There are others and each is considered to be
+              Examples of Central American boa locales are Hogg Island, El
+              Salvador (where the &quot;blood morph originated&quot;), Nicaraguan, and Costa Rica. There are others and each is considered to be
               it&apos;s own &quot;locale&quot;, there are many breeders who
               specialize in breeding pure locale boas from each of these
-              regions. My two Blood Boas are pure Central American Boas, the
-              others in my collection have small amounts of South American BCI
-              blood.
+              regions. Most of my blood boa project is comprised of pure Central American boas but I do have a couple that are mixed. The albino and jungle morphs for example do not happen in any of the Central American locales so in order to introduce that you have to mix in some Colombian genetics. 
             </p>
             <div className="columns is-multiline section">
               {collectionCATiles}
@@ -157,12 +151,12 @@ const CollectionContainer = () => {
           <div className="column">
             <h1 className="is-size-2">Argentine Boa Collection</h1>
             <p className="is-size-7">
-              If I had more space I would absolutey have more sub-species of
+              If I had more space I would absolutely have more sub-species of
               boas. Like I&apos;d probably have all of them and all the locales.
               But space is limited and taking care of 20+ boas is already a lot
               of work for one person so I have to maintain some semblance of
               self control. Currently the only sub-species I own is a pair of
-              Argentine Boas which are Boa Constrictor Occidentalis. Both of
+              Argentine Boas which are Boa constrictor occidentalis. Both of
               them were purchased from Ancient Reproductions who are super nice
               and wonderful breeders of Argentine Boas.
             </p>
--- app/javascript/components/Home/AboutUs.js
+++ app/javascript/components/Home/AboutUs.js
@@ -6,15 +6,13 @@ const AboutUs = () => {
       <h3 className="is-size-3">About Us</h3>
       <p className="is-size-7">
         Outworld Reptiles is owned by Maddox Grey, a long time reptile
-        enthusiast. Our current projects are based around Boa Constrictors;
-        including several different BCI morphs, Blood Boas, and a small BCO
-        project revolving around a high pink line of Argentine Boas, please
-        refer to the Collections page for more information.
+        enthusiast. Our goal is to breed quality healthy boas. Current projects include a variety of borphs, a project focused on the Central American blood morph, finally a small Maxx Pink Line Argentine Boa project. Please check out our <a href="/collection">Collections</a> page for more info!
       </p>
+      <br />
       <p className="is-size-7">
         We typically only have a couple litters a year, once the babies are
         established they will be posted on <a href="https://www.morphmarket.com/stores/outworldreptiles/" target="_blank" rel="noreferrer">Morph Market</a>. If you have any
-        questions please contact us at outworldreptiles@gmail.com.
+        questions please contact us at <a href="mailto:outworldreptiles@gmail.com">outworldreptiles@gmail.com</a>.
       </p>
       <br />
       <h5 className="is-size-5 has-text-centered">Follow us on Instagram or Facebook!</h5>
